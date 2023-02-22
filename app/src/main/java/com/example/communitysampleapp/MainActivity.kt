5c1bbf91231fe8677283d098c8d32a9e1954b902
최현정 <hjchoi.dev@gmail.com>
2023-02-22 14:34:06 +0900

    ✨ Add Databinding

--- app/src/main/java/com/example/communitysampleapp/MainActivity.kt
+++ app/src/main/java/com/example/communitysampleapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.communitysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.communitysampleapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnMainSignUp.setOnClickListener {
+            val email = binding.etMainEmail.text.toString()
+            val password = binding.etMainPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+
+                    } else {
+
+                    }
+            }
+
+        }
     }
 }
