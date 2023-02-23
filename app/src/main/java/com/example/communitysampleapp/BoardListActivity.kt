8ca750079bf0671c63eca5629ea661cf66e51559
최현정 <hjchoi.dev@gmail.com>
2023-02-23 14:32:44 +0900

    :sparkles: Add Activities

---
+++ app/src/main/java/com/example/communitysampleapp/BoardListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.communitysampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_list)
+    }
+}
