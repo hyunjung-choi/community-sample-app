02ecfde711494959aee2abb94cf88846a6633d53
최현정 <hjchoi.dev@gmail.com>
2023-02-24 16:12:52 +0900

    :sparkles: Add Write Button

--- app/src/main/java/com/example/communitysampleapp/BoardListActivity.kt
+++ app/src/main/java/com/example/communitysampleapp/BoardListActivity.kt
@@ -1,11 +1,22 @@
 package com.example.communitysampleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.communitysampleapp.databinding.ActivityBoardListBinding
 
 class BoardListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBoardListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_list)
+        setContentView(binding.root)
+
+        binding.btnListWrite.setOnClickListener {
+            val intent = Intent(this, BoardWriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
