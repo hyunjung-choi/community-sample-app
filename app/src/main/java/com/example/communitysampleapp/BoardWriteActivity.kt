f319a0b0f99e0ffb4cba8728192dc7a68058e2b3
최현정 <hjchoi.dev@gmail.com>
2023-02-24 16:35:04 +0900

    :sparkles: Add Realtime Database

--- app/src/main/java/com/example/communitysampleapp/BoardWriteActivity.kt
+++ app/src/main/java/com/example/communitysampleapp/BoardWriteActivity.kt
@@ -2,10 +2,27 @@ package com.example.communitysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.communitysampleapp.databinding.ActivityBoardWriteBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class BoardWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBoardWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_write)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
+        setContentView(binding.root)
+
+        binding.btnBoardWrite.setOnClickListener {
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+            val text = binding.etBoardWrite.text.toString()
+
+            myRef.push().setValue(
+                Model(text)
+            )
+        }
     }
 }
