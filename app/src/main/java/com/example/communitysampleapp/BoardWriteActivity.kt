58ad58764df00e4885982d50e16e7f543bada0d0
최현정 <hjchoi.dev@gmail.com>
2023-03-08 22:22:53 +0900

    ✨ Add Checking Null in BoardWriteActivity

--- app/src/main/java/com/example/communitysampleapp/BoardWriteActivity.kt
+++ app/src/main/java/com/example/communitysampleapp/BoardWriteActivity.kt
@@ -2,6 +2,7 @@ package com.example.communitysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.databinding.DataBindingUtil
 import com.example.communitysampleapp.databinding.ActivityBoardWriteBinding
 import com.google.firebase.database.ktx.database
@@ -21,10 +22,22 @@ class BoardWriteActivity : AppCompatActivity() {
             val myRef = database.getReference("board")
             val text = binding.etBoardWrite.text.toString()
 
-            myRef.push().setValue(
-                Model(text)
-            )
-            finish()
+            if (checkWriteIsNull(text)) {
+                return@setOnClickListener
+            } else {
+                myRef.push().setValue(
+                    Model(text)
+                )
+                finish()
+            }
         }
+    }
+
+    private fun checkWriteIsNull(text: String): Boolean {
+        if (TextUtils.isEmpty(text)) {
+            binding.etBoardWrite.setError("You did not write any text.")
+            return true
+        }
+        return false
     }
 }
